580f5230b601898a7a6ea09504579394bae57107
KengYuChang <kengyuchang@gmail.com>
2021-03-12 19:21:05 +0800

    firt

--- python-flsk-todo-app/config.py
+++ python-flsk-todo-app/config.py
@@ -1,6 +1,18 @@
-class Config(object):
-    pass
-class ProdConfig(Config):
-    pass
-class DevConfig(Config):
-    DEBUG = True
+#18.114(DB_web)
+DB_web = {'SERVER' : '10.93.18.114'
+         ,'USER' : 'scheduler'
+         ,'PASSWORD' : 'sch@8888'
+         ,'DATABASE' : 'CRAWLER'}
+
+#DB_conn
+DB_conn = {'123_server' : '10.93.3.123'
+         ,'123_user' : 'CATHAYSEC\F126191299'
+         ,'123_password' : '!QAZ2wsx'
+         ,'125_server' : '10.93.3.125'
+         ,'125_user' : 'CATHAYSEC\F126191299'
+         ,'125_password' : '!QAZ2wsx'
+         ,'115_server' : '10.93.18.115'
+         ,'115_user' : 'CATHAYSEC\F126191299'
+         ,'115_password' : '!QAZ2wsx'
+         }
+
